bdcadeb5fc4b9642669781feea26063335a6b19f
Piyushkashya <piyush8580kashyap@gmail.com>
2024-06-24 19:35:53 +0530

    feat: fix input box

--- src/components/chat.tsx
+++ src/components/chat.tsx
@@ -1,29 +1,28 @@
-import React from 'react'
+import React from "react";
 
-const chat = () => {
-    return (
-        <div className="absolute">
-            <div className="fixed bottom-16 right-16 rounded-full bg-red-500 h-20 w-20 flex items-center justify-center">
-                icon
-            </div>
+const Chat = () => {
+  return (
+    <div className="absolute">
+      <div className="fixed bottom-16 right-16 rounded-full bg-gradient-to-r from-red-300 to-orange-500 h-20 w-20 flex items-center justify-center">
+        icon
+      </div>
 
-            <div className='bg-gradient-to-r from-orange-300 to-blue-300 h-[600px] w-[400px] fixed bottom-24 right-20'>
-
-                <div>
-                    <p className='p-8 text-2xl font-mono '> ChatBot </p>
-
-                    <div className='bg-black border-t'> </div>
-                </div>
-
-                <div className=' flex justify-center place-items-end '>
-                    <input className="bg-slate-400 w-full mx-4 rounded-lg" type="text" />
-                </div>
-
-            </div>
-
-
-
+      <div className="bg-gradient-to-r from-indigo-400 to-blue-300 h-[600px] w-[400px] fixed bottom-24 right-20 flex flex-col justify-between">
+        <div>
+          <p className="p-8 text-2xl font-mono">ChatBot</p>
+          <div className="bg-black border-t"></div>
         </div>
-    )
-}
-export default chat;
+
+        <div className="flex justify-center p-4">
+          <input
+            className="bg-slate-200 h-12 w-full mx-4 rounded-sm"
+            type="text"
+            placeholder="type here"
+          />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Chat;
